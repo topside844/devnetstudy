f33322f6629c32b85646b0c27b5cadf6bdeaea6e
Dustin Melancon <topside844@gmail.com>
2020-05-14 15:17:56 -0500

    Code refactoring

--- challenge7/merakicheck.py
+++ challenge7/merakicheck.py
@@ -12,9 +12,6 @@ mxversion = '15.27'
 mvversion = '4.0'
 WebexRoomID = "Y2lzY29zcGFyazovL3VzL1JPT00vNWJiMmRiZjAtNmFkOC0xMWVhLWEzNmEtMDc0ZjMxN2Y0Njli"
 myWebexToken = "" #you will need to put your personal token here
-mscount = mrcount = mxcount = mvcount = 0
-bad_devices = []
-
 baseurl = "https://dashboard.meraki.com/api/v0/networks/"
 
 payload = {}
@@ -25,30 +22,33 @@ url = baseurl + mynetwork + "/devices"  #finish the url!
 response = requests.get(url, headers=headers) #complete the api call using the requests library
 myresponse = response.json()
 
+device_count = {'ms': 0, 'mr': 0, 'mx': 0, 'mv': 0}
+bad_devices = []
+
 for device in myresponse:
     firmware = re.sub('\D+(.*)', '\\1', device['firmware'])
     firmware = firmware.replace('-', '.')
     device_type = device['model'][:2]
 
     if device_type == "MS" and firmware == msversion:
-        mscount += 1
+        device_count['ms'] += 1
     elif device_type == "MR" and firmware == mrversion:
-        mrcount += 1
+        device_count['mr'] += 1
     elif device_type == "MX" and firmware == mxversion:
-        mxcount += 1
+        device_count['mx'] += 1
     elif device_type == "MV" and firmware == mvversion:
-        mvcount += 1
+        device_count['mv'] += 1
     else:
         bad_devices.append(device)
 
-print(f"Total switches that meet standard: {mscount}")
-print(f"Total APs that meet standard: {mrcount}")
-print(f"Total Security Appliances that meet standard: {mxcount}")
-print(f"Total Cameras that meet standard: {mvcount}")
+print(f"Total switches that meet standard: {device_count['ms']}")
+print(f"Total APs that meet standard: {device_count['mr']}")
+print(f"Total Security Appliances that meet standard: {device_count['mx']}")
+print(f"Total Cameras that meet standard: {device_count['mv']}")
 print(f"Devices that will need to be manually checked:")
 
 for device in bad_devices:
     print(f"Serial#: {device['serial']}, Model#: {device['model']}")
 
-wx_api = WebexTeamsAPI(access_token=myWebexToken)
-wx_api.messages.create(WebexRoomID, text="Report Completed!")
+#wx_api = WebexTeamsAPI(access_token=myWebexToken)
+#wx_api.messages.create(WebexRoomID, text="Report Completed!")
